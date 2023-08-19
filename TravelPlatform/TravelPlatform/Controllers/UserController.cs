62fa5ba96e7fe9ec3c0a6d3a255768b28beb3f89
shiru19 <afa890419@gmail.com>
2023-08-20 00:14:17 +0800

    feat: Get user list api

---
+++ TravelPlatform/TravelPlatform/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TravelPlatform.Models.Domain;
+using TravelPlatform.Services;
+
+namespace TravelPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class UserController : ControllerBase
+    {
+        private readonly TravelContext _db;
+
+        public UserController(TravelContext db)
+        {
+            _db = db;
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpGet("GetUserList")]
+        public IActionResult GetUserList()
+        {
+            try
+            {
+                var user = _db.Users.Select(u => new
+                {
+                    id = u.Id,
+                    name = u.Name,
+                    sex = u.Sex,
+                    birthday = u.Birthday,
+                    email = u.Email,
+                    phoneNumber = u.PhoneNumber,
+                    region = u.Region
+                });
+                return Ok(user);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
