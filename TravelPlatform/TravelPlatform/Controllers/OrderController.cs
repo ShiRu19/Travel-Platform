2a0c404c1db1b34de3dd1cf2109e1b8fe1369ba4
shiru19 <afa890419@gmail.com>
2023-08-30 21:25:27 +0800

    feat: Get order list API for backstage

---
+++ TravelPlatform/TravelPlatform/Controllers/OrderController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TravelPlatform.Models.Domain;
+using TravelPlatform.Models.Order;
+
+namespace TravelPlatform.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    public class OrderController : ControllerBase
+    {
+        private readonly TravelContext _db;
+        public OrderController(TravelContext db)
+        {
+            _db = db;
+        }
+
+        [MapToApiVersion("1.0")]
+        [HttpGet("GetOrderList")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetOrderList()
+        {
+            return Ok(new
+            {
+                order_unchecked = GenerateOrderList(0),
+                order_checked = GenerateOrderList(1),
+                order_canceled = GenerateOrderList(2)
+            });
+        }
+
+        private List<OrderListDto> GenerateOrderList(int checkStatus)
+        {
+            var orders = _db.Orders.Where(o => o.Check == checkStatus)
+                                                .OrderBy(o => o.OrderDate)
+                                                .ToList();
+
+            List<OrderListDto> OrderList = new List<OrderListDto>();
+
+            foreach (var order in orders)
+            {
+                var travelSession = _db.TravelSessions.Where(t => t.Id == order.TravelSessionId).Single();
+                var user = _db.Users.Where(u => u.Id == order.UserId).Single();
+                var qty = _db.OrderLists.Where(o => o.OrderId == order.Id).Count();
+
+                OrderListDto orderDto = new OrderListDto()
+                {
+                    OrderId = order.Id,
+                    ProductNumber = travelSession.ProductNumber,
+                    Qty = qty,
+                    Total = travelSession.Price * qty,
+                    UserName = user.Name,
+                    UserEmail = user.Email,
+                    OrderDate = order.OrderDate
+                };
+
+                if(checkStatus != 0)
+                {
+                    orderDto.CheckDate = order.CheckDate;
+                }
+
+                OrderList.Add(orderDto);
+            }
+
+            return OrderList;
+        }
+    }
+}
---
+++ TravelPlatform/TravelPlatform/Models/Order/OrderListDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TravelPlatform.Models.Order
+{
+    public class OrderListDto
+    {
+        public long OrderId { get; set; }
+        public string ProductNumber { get; set; } = null!;
+        public int Qty { get; set; }
+        public int Total { get; set; }
+        public string UserName { get; set; } = null!;
+        public string UserEmail { get; set; } = null!;
+        public DateTime OrderDate { get; set; }
+        public DateTime? CheckDate { get; set; }
+    }
+}
